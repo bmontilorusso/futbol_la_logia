a23b8c2876aa9a0b5474845d2547dd6576a98823
bmontilorusso <b.montilorusso@gmail.com>
2025-07-11 04:01:45 -0300

    Inicio de "Abrir Convocatoria".

--- php/home.php
+++ php/home.php
@@ -1,3 +1,8 @@
+<?php
+    include('validacion_sesion.php');
+    include('conexion_db.php');    
+?>
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -17,8 +22,55 @@
 
 <body>
 
-    <h1>Home</h1>
+    <div class="ventana-aplicacion">
+
+        <header class="header">
+            <h1>La Logia Fútbol</h1>
+        </header>
+
+        <main class="main">
+
+            <button class="boton">Abrir convocatoria ⚽</button>
+
+            <form class="formulario-convocatoria" action="php/convocatoria.php" method="POST">
+                <div class="campos">
+                    <label class="label" for="">Fecha</label>
+                    <input class="input" type="date" placeholder="Fecha" autocomplete="off" required>
+
+                    <label class="label" for="">hora</label>
+                    <input class="input" type="time" placeholder="Hora" autocomplete="off" required>
+
+                    <label class="label" for="">Estadio</label>
+                    <select class="input" name="estadio" id="" required>
+                        <option value="" disabled selected>-Seleccione un Estadio-</option>
+                        <?php
+                            // Conexión a la DB, al comienzo.
+                            $sqlEstadios = "Select * from ESTADIOS;";
+                            $resultadoEstadios = mysqli_query($conn, $sqlEstadios);
+                        ?>
+                        <?php while($fila = mysqli_fetch_assoc($resultadoEstadios)): ?>
+                            <option value="<?php echo $fila['ID_ESTADIO'] ?>">
+                                <?php echo $fila['NOMBRE']; ?>
+                            </option>
+                        <?php endwhile; ?>
+                    </select>
+
+                    <label for="">¿Confirmo asistencia?</label>
+                    <input type="checkbox" name="" id="">
+
+
+                </div>
+            </form>           
+
+        </main>
+    </div> <!-- Fin de la Ventana-aplicación -->
     
+    <footer class="footer">
+        <p>© 2025 Holly Molly Studios • Desarrollado con 💾, ❤️ y mucho ☕</p>
+    </footer>
+
+    <!-- Script: -->
+    <script src="js/script.js"></script>    
     
 </body>
 </html>
